e66fe48ee695f6262bf2af6d777d3f3d7bed433c
rooij_e <erik.derooij@deltares.nl>
2022-02-08 14:10:40 +0100

    Added SecurityHeadersProvider to enable 'referrerPolicy' in response headers of POST request

---
+++ deltares-extension-module/src/main/java/nl/deltares/keycloak/authentication/headers/SamlSecurityHeadersProvider.java
@@ -0,0 +1,50 @@
+package nl.deltares.keycloak.authentication.headers;
+
+import org.keycloak.headers.DefaultSecurityHeadersProvider;
+import org.keycloak.models.BrowserSecurityHeaders;
+import org.keycloak.models.KeycloakSession;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collections;
+
+public class SamlSecurityHeadersProvider extends DefaultSecurityHeadersProvider {
+
+    private String referrerPolicy = "strict-origin-when-cross-origin";
+
+    public SamlSecurityHeadersProvider(KeycloakSession session) {
+        super(session);
+    }
+
+    @Override
+    public void addHeaders(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        super.addHeaders(requestContext, responseContext);
+
+        if (isSamlLogin(requestContext)) {
+            replaceReferrerHeader(responseContext);
+        }
+    }
+
+    public void setReferrerPolicy(String configuredValue){
+        this.referrerPolicy = configuredValue;
+    }
+
+    private boolean isSamlLogin(ContainerRequestContext requestContext) {
+        return requestContext.getMethod().equals("POST");
+    }
+
+    /**
+     * Replace the default Refferrer Policy from 'no-referres' to 'strict-origin-when-cross-origin'.
+     * <p>
+     * Do this only for the SAML logins.
+     *
+     * @param responseContext Response context containing headers
+     */
+    private void replaceReferrerHeader(ContainerResponseContext responseContext) {
+        final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.replace(BrowserSecurityHeaders.REFERRER_POLICY.getHeaderName(), Collections.singletonList(referrerPolicy));
+    }
+
+
+}
