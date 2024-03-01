c08a0c2c7792e2926e97a55c9087565b86fb5273
rooije <erik.derooij@deltares.nl>
2024-03-01 13:50:25 +0100

    Adding Integration testing

--- mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
+++ mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
@@ -23,6 +23,8 @@ public class IntegrationTestSuite {
     public static void startKeyCloak() throws Throwable {
 
         final File testResources = Paths.get( "src", "test", "resources", "docker").toFile();
+        final File buildLibs = Paths.get( "build", "libs").toFile();
+
         File keycloakTmpDir = Files.createTempDirectory("keycloak").toFile();
 
         File dataDir = new File(keycloakTmpDir, "keycloak/data/import");
@@ -35,8 +37,8 @@ public class IntegrationTestSuite {
         Files.copy(new File(testResources, "docker-compose.yml").toPath(), new File(keycloakTmpDir, "docker-compose.yml").toPath());
         Files.copy(new File(testResources, "keycloak/data/import/realm-export.json").toPath(), new File(dataDir, "realm-export.json").toPath());
         Files.copy(new File(testResources, "keycloak/conf/keycloak.conf").toPath(), new File(configDir, "keycloak.conf").toPath());
-        Files.copy(new File(testResources, "keycloak/providers/mydeltares-keycloak-spi-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-spi-3.0.jar").toPath());
-        Files.copy(new File(testResources, "keycloak/providers/mydeltares-keycloak-theme-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-theme-3.0.jar").toPath());
+        Files.copy(new File(buildLibs, "mydeltares-keycloak-spi-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-spi-3.0.jar").toPath());
+//        Files.copy(new File(testResources, "keycloak/providers/mydeltares-keycloak-theme-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-theme-3.0.jar").toPath());
 
         KeycloakTestServer.startKeycloak(keycloakTmpDir.getPath());
 
