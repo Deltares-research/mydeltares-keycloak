46a7aed6f3b65bc8d17818f6809ec8ee0f23695a
rooije <erik.derooij@deltares.nl>
2024-03-07 12:55:43 +0100

    Add e-mail 2FA

---
+++ mydeltares-keycloak-spi/src/main/java/nl/deltares/keycloak/authentication/authenticators/email/EmailConstants.java
@@ -0,0 +1,10 @@
+package nl.deltares.keycloak.authentication.authenticators.email;
+
+
+public class EmailConstants {
+	public static String CODE = "emailCode";
+	public static String CODE_LENGTH = "length";
+	public static String CODE_TTL = "ttl";
+	public static int DEFAULT_LENGTH = 6;
+	public static int DEFAULT_TTL = 300;
+}
