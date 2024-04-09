c13144eed45e824a9799eabbbdeb6dbe863088e2
rooije <erik.derooij@deltares.nl>
2024-04-09 15:41:32 +0200

    Fixing build for Github actions

--- mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
+++ mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
@@ -6,9 +6,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 
 @SelectPackages({"nl.deltares.keycloak"})
@@ -22,11 +20,6 @@ public class IntegrationTestSuite {
     public static void startKeyCloak() throws Throwable {
 
         final File testResources = Paths.get( "src", "test", "resources", "docker").toFile();
-        final File buildLibs = Paths.get( "build", "libs").toFile();
-
-       Files.copy(new File(buildLibs, "mydeltares-keycloak-spi-3.0.jar").toPath(),
-                new File(testResources, "/keycloak/providers/mydeltares-keycloak-spi-3.0.jar").toPath(), StandardCopyOption.REPLACE_EXISTING);
-
         KeycloakTestServer.startKeycloak(testResources.getPath());
 
     }
