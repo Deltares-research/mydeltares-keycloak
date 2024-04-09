fef7c0267640db9b13046fd2732d541e87ce21ae
rooije <erik.derooij@deltares.nl>
2024-04-09 14:57:03 +0200

    Fixing build for Github actions

--- mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
+++ mydeltares-keycloak-spi/src/test/java/nl/deltares/keycloak/IntegrationTestSuite.java
@@ -1,7 +1,6 @@
 package nl.deltares.keycloak;
 
 import nl.deltares.keycloak.utils.KeycloakTestServer;
-import org.junit.BeforeClass;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
@@ -9,8 +8,8 @@ import org.junit.platform.suite.api.Suite;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SelectPackages({"nl.deltares.keycloak"})
 @IncludeTags("IntegrationTestCategory")
@@ -25,22 +24,10 @@ public class IntegrationTestSuite {
         final File testResources = Paths.get( "src", "test", "resources", "docker").toFile();
         final File buildLibs = Paths.get( "build", "libs").toFile();
 
-        File keycloakTmpDir = Files.createTempDirectory("keycloak").toFile();
+       Files.copy(new File(buildLibs, "mydeltares-keycloak-spi-3.0.jar").toPath(),
+                new File(testResources, "/keycloak/providers/mydeltares-keycloak-spi-3.0.jar").toPath(), StandardCopyOption.REPLACE_EXISTING);
 
-        File dataDir = new File(keycloakTmpDir, "keycloak/data/import");
-        dataDir.mkdirs();
-        File configDir = new File(keycloakTmpDir, "keycloak/conf");
-        configDir.mkdir();
-        File deploymentDir = new File(keycloakTmpDir, "keycloak/providers");
-        deploymentDir.mkdir();
-
-        Files.copy(new File(testResources, "docker-compose.yml").toPath(), new File(keycloakTmpDir, "docker-compose.yml").toPath());
-        Files.copy(new File(testResources, "keycloak/data/import/realm-export.json").toPath(), new File(dataDir, "realm-export.json").toPath());
-        Files.copy(new File(testResources, "keycloak/conf/keycloak.conf").toPath(), new File(configDir, "keycloak.conf").toPath());
-        Files.copy(new File(buildLibs, "mydeltares-keycloak-spi-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-spi-3.0.jar").toPath());
-//        Files.copy(new File(testResources, "keycloak/providers/mydeltares-keycloak-theme-3.0.jar").toPath(), new File(deploymentDir, "mydeltares-keycloak-theme-3.0.jar").toPath());
-
-        KeycloakTestServer.startKeycloak(keycloakTmpDir.getPath());
+        KeycloakTestServer.startKeycloak(testResources.getPath());
 
     }
 
